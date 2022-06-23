ed5c699ded3955fb697cf735a07bb55a0c15000f
unknown <jackhartwig7@gmail.com>
2022-06-23 00:22:09 -0500

    rewored everything

--- components/project.js
+++
@@ -1,9 +0,0 @@
-export default function Project() {
-  return (
-      <div className="Project">
-          <div className="box">
-            <h1>Project Name</h1>
-          </div>
-      </div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import NavBar from '../components/NavBar'
-import HomeBody from '../components/HomeBody'
-import InfiniteCarousel from '../components/InfiniteCarousel'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Jack Hartwig</title>
-        <meta name="description" content="Jack Hartwig Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <NavBar></NavBar>
-      <HomeBody></HomeBody>
-      {/* <InfiniteCarousel></InfiniteCarousel> */}
+      <img className='grid' src='/grid.webp'></img>
+      <div className='navbar'>
+        <div className='buttons'>
+          <h1>Home</h1>
+          <h1>Contact</h1>
+          <h1>Resume</h1>
+        </div>
+      </div>
+      <div className='largeName'>
+        <h1>Jack Hartwig</h1>
+      </div>
+      <div className='bio'>
+        <h1>A Full Stack</h1>
+        <h1>Web Dev</h1>
+      </div>
     </div>
   )
 }
